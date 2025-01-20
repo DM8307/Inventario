72d5def4a28d7bc2d41fb6ac76a5d3e284af3406
Danny Mendoza <dannyemendoza@hotmail.com>
2025-01-19 23:39:39 -0400

    muestran catergorias

--- Inventario/Views/Categorias.cs
+++ Inventario/Views/Categorias.cs
@@ -7,20 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Inventario.Context;
 using Inventario.Models;
 
 namespace Inventario.Views
 {
     public partial class Categorias : Form
     {
+        private InventarioContext _context;
         public Categorias()
         {
             InitializeComponent();
+            _context = new InventarioContext();
         }
 
         private void Categorias_Load(object sender, EventArgs e)
         {
+            CargarCategorias();
+        }
+        private void CargarCategorias()
+        {
+            // Cargar las categorías desde la base de datos
+            var categorias = _context.Categorias.ToList();
 
+            // Asignar las categorías al BindingSource
+            categoriaBindingSource.DataSource = categorias;
+
+            // Vincular el BindingSource al DataGridView
+            dtgvCategorias.DataSource = categoriaBindingSource;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -88,6 +102,16 @@ namespace Inventario.Views
         {
             // Volver al menú principal
             this.Close(); // Cierra el formulario de categorias
+        }
+
+        private void button1agregarproductos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Categorias_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _context.Dispose();
         }
     }
 }
